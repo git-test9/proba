aead63754cc38d0af8380be5f2328c7ad636db42
git-test9 <gittest9@gmail.com>
2016-03-14 20:10:54 +0100

    Dodati geteri i seteri u klasu Knjiga.

--- Proba/src/klase/Knjiga.java
+++ Proba/src/klase/Knjiga.java
@@ -1,9 +1,26 @@
 package klase;
 
 public class Knjiga {
-	
+
 	private String naziv;
 	private String isbn;
+
 	
+	
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
 
 }
